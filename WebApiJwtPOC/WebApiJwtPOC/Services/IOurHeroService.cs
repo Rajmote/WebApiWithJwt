e37e0ef2cfe70e4b35f5bc95bd827bb9a9c9e392
Rajmote <rajdmote@outlook.com>
2024-11-10 14:20:27 +0530

    Added crud operaitons

---
+++ WebApiJwtPOC/WebApiJwtPOC/Services/IOurHeroService.cs
@@ -0,0 +1,17 @@
+﻿using WebApiJwtPOC.Model;
+
+namespace WebApiJwtPOC.Services
+{
+    public interface IOurHeroService
+    {
+        List<OurHero> GetAllHeros(bool? isActive);
+
+        OurHero? GetHerosByID(int id);
+
+        OurHero AddOurHero(AddUpdateOurHero obj);
+
+        OurHero? UpdateOurHero(int id, AddUpdateOurHero obj);
+
+        bool DeleteHerosByID(int id);
+    }
+}
